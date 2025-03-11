b12053ca1b0d37c4588eb0a5bbe701e3b51a58f2
CARLOS-012 <160601956+CARLOS-012@users.noreply.github.com>
2025-03-11 09:42:14 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Tropic-lia
+# tropic-lia
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-document.addEventListener(`DOMContentLoaded`, function (){
-    const aumentaFonteBotao = document.getElementById(`aumentar-fonte`);
-    const diminuirFomteBotao = document.getElementById(`diminuir-fonte`);
+document.addEventListener('DOMContentLoaded',function (){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
      let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener(`click`, function(){
-        tamanhoAtualFonte += 0.1;
+       aumentaFonteBotao.addEventListener('click',function(){
+         tamanhoAtualFonte += 0.1;
+          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
+       diminuirFonteBotao.addEventListener('click',function(){
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-    
+       })
 })
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcesibilidade = document.getElementById('opcoes-acessibilidade');
+  botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcesibilidade.classList.toggle('apresenta-lista');
+  })
