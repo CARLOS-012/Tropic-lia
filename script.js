cdbc9c76b24cc42bf17c2868262dea90c2aa4c3f
CARLOS-012 <160601956+CARLOS-012@users.noreply.github.com>
2025-03-28 09:47:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded',function (){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
-    const alternaContraste = document.getElementById('alterna-contraste')
+    const aumentaFonteBotao = document.getElementById
+    ('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById
+    ('diminuir-fonte');
+    const alternaContraste = document.getElementById
+    ('alterna-contraste');
+  
      let tamanhoAtualFonte = 1;
 
        aumentaFonteBotao.addEventListener('click',function(){
@@ -18,8 +22,20 @@ const opcoesDeAcesibilidade = document.getElementById('opcoes-acessibilidade');
   botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcesibilidade.classList.toggle('apresenta-lista');
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute
+    ('aria-expanded') === 'true';
+      botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado);
   })
-  alternaContraste.addEventListener ('click',function(){
+  alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
-  })
 })
+})
+    ScrollReveal().reveal('.#inicio', { delay: 500 });
+    ScrollReveal().reveal('.#tropicalia', { delay: 500 });
+    ScrollReveal().reveal('.#galeria', { delay: 500 });
+    ScrollReveal().reveal('.#contato', { delay: 500 });
+
+
+  
+
+
